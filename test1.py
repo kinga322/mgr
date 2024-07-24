197481617cd8576be5f7bda70d723de7bfdcaa43
kinga322 <kinga@szyman.org.pl>
2024-07-24 10:34:41 +0200

    times

--- test1.py
+++ test1.py
@@ -1,10 +1,21 @@
 import scanpy as sc
-import anndata
+import os
+import time
 
-pr1 = sc.read_10x_mtx("/home/kszyman/Downloads/GSE161529_RAW/GSM4909321_mER-MH0068-LN-matrix.mtx/")
+pca_times={}
 
-sc.pp.pca(pr1)
-sc.pp.neighbors(pr1)
-sc.tl.leiden(pr1, flavor="igraph", n_iterations=2, directed=False)
-sc.external.tl.phate(pr1)
-sc.pl.embedding(pr1, "X_phate", color="leiden")
+subfolders = [f.path for f in os.scandir("/home/kszyman/Downloads/GSE161529_RAW") if f.is_dir()]
+for subdir in subfolders:
+    adata = sc.read_10x_mtx(subdir)
+    start = time.time()
+    sc.external.tl.phate(adata)
+    end = time.time()
+    phate_time = end - start
+    start = time.time()
+    sc.pp.pca(adata)
+    end = time.time()
+    pca_time = end - start
+# sc.pp.neighbors(pr1)
+# sc.tl.leiden(pr1, flavor="igraph", n_iterations=2, directed=False)
+
+# sc.pl.embedding(pr1, "X_phate", color="leiden")
