b16c708359dcfac00ed1a981a20ed525769363eb
kinga322 <kinga@szyman.org.pl>
2024-07-24 10:45:04 +0200

    times

--- test1.py
+++ test1.py
@@ -6,15 +6,23 @@ pca_times={}
 
 subfolders = [f.path for f in os.scandir("/home/kszyman/Downloads/GSE161529_RAW") if f.is_dir()]
 for subdir in subfolders:
-    adata = sc.read_10x_mtx(subdir)
-    start = time.time()
-    sc.external.tl.phate(adata)
-    end = time.time()
-    phate_time = end - start
-    start = time.time()
-    sc.pp.pca(adata)
-    end = time.time()
-    pca_time = end - start
+    try:
+        adata = sc.read_10x_mtx(subdir)
+    except:
+        pass
+    if adata:
+        sc.pp.filter_cells(adata, min_genes=10)
+        start = time.time()
+        sc.external.tl.phate(adata)
+        end = time.time()
+        phate_time = end - start
+        start = time.time()
+        sc.pp.pca(adata)
+        end = time.time()
+        pca_time = end - start
+        pca_times[adata.n_vars] = pca_time
+
+print(pca_times)
 # sc.pp.neighbors(pr1)
 # sc.tl.leiden(pr1, flavor="igraph", n_iterations=2, directed=False)
 
