a1807d87a115cc08dfa0ad3815075a103e572963
kinga322 <kinga@szyman.org.pl>
2024-07-26 14:00:09 +0200

    HDBSCAN

--- clustering.py
+++ clustering.py
@@ -1,7 +1,7 @@
 import scanpy as sc
 import matplotlib.pyplot as plt
 import time
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, HDBSCAN
 from helpers import load_adatas
 
 
@@ -17,6 +17,11 @@ def cluster(adata, method="leiden"):
         X_pca = adata.obsm['X_pca']
         kmeans = KMeans(n_clusters=2, random_state=42).fit(X_pca)
         adata.obs['kmeans'] = kmeans.labels_.astype(str)
+    elif method == "HDBSCAN":
+        X_pca = adata.obsm['X_pca']
+        hdb = HDBSCAN(min_cluster_size=10)
+        hdb.fit(X_pca)
+        adata.obs['hdbscan'] = hdb.labels_
     end = time.time()
     elapsed = end - start
     return adata, elapsed
