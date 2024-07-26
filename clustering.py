3ad65862864be46ab33365e6ab4882854b3b2fd8
kinga322 <kinga@szyman.org.pl>
2024-07-26 14:42:04 +0200

    DBSCAN

--- clustering.py
+++ clustering.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import time
 from sklearn.cluster import KMeans, HDBSCAN
 from helpers import load_adatas
-
+import numpy as np
 
 def cluster(adata, method="leiden"):
     start = time.time()
@@ -18,10 +18,13 @@ def cluster(adata, method="leiden"):
         kmeans = KMeans(n_clusters=2, random_state=42).fit(X_pca)
         adata.obs['kmeans'] = kmeans.labels_.astype(str)
     elif method == "HDBSCAN":
+        dense = np.asarray(adata.X.todense())
+        adata.X = dense
         X_pca = adata.obsm['X_pca']
         hdb = HDBSCAN(min_cluster_size=10)
         hdb.fit(X_pca)
-        adata.obs['hdbscan'] = hdb.labels_
+        labels = [str(x) for x in hdb.labels_]
+        adata.obs['hdbscan'] = labels
     end = time.time()
     elapsed = end - start
     return adata, elapsed
